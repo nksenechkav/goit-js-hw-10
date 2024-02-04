ef5021d5926d46a6d05a2c7201b77af6f090105e
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2024-02-04 09:15:17 +0000

    Deploying to gh-pages from @ nksenechkav/goit-js-hw-10@b5501cd06ccac561518789ee74668bc198daf652 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";function c(e,o,t){return new Promise((i,r)=>{const n={value:e,delay:o};setTimeout(()=>{t?i(n):r(n)},o)})}for(let e=0;e<5;e++){const o=e*1e3,t=Math.random()>.5;c(e,o,t).then(onFullFiled,onRejected)}
+//# sourceMappingURL=commonHelpers2.js.map
